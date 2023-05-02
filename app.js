2c8c11a8883815208b508d35ac9628698add1ae0
ashish1141 <ashishgoyal1141@.com>
2023-05-02 21:31:54 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const validator= require('validator');
+const chalk = require('chalk');
+const getNotes = require('./notes.js');
+
+const msg = getNotes();
+
+console.log(msg);
+const k = validator.isEmail('corre');
+
+console.log(chalk.inverse.green(k));
